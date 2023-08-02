79bd9a19014cfd646cd0be892030cdcd1da7a709
Reza Teshnizi <rht1369@gmail.com>
2023-08-02 01:57:21 -0500

    chore(__init__): Add some refs to package headers

--- src/rt_bi_core/rt_bi_core/Model/__init__.py
+++ src/rt_bi_core/rt_bi_core/Model/__init__.py
@@ -1,4 +1,4 @@
-"""The data model"""
+"""The data model."""
 from rt_bi_core.Model.Fov import Events, Fov
 from rt_bi_core.Model.MapRegion import MapRegion, PolygonalRegion
 from rt_bi_core.Model.SensingRegion import SensingRegion
---
+++ src/rt_bi_utils/rt_bi_utils/__init__.py
@@ -0,0 +1,3 @@
+from rt_bi_utils.Geometry import Geometry, LineString, MultiPolygon, Point, Polygon
+from rt_bi_utils.RViz import Color, KnownColors, RViz
+from rt_bi_utils.SaMsgs import SaMsgs
