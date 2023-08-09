f0ca5e223bca1c7307caae6d578115c4dbc96fef
Reza Teshnizi <rht1369@gmail.com>
2023-08-08 23:35:00 -0500

    chore(__init__): Remove all imports to avoid circular dependencies

--- src/rt_bi_core/rt_bi_core/ShadowTree/__init__.py
+++ src/rt_bi_core/rt_bi_core/ShadowTree/__init__.py
@@ -1,6 +1,3 @@
 """
 The code related to Shadow Tree data structure.
 """
-from rt_bi_core.ShadowTree.ShadowTree import ShadowTree
-from rt_bi_core.ShadowTree.ConnectivityGraph import ConnectivityGraph
-from rt_bi_core.ShadowTree.Graph import GraphAlgorithms
--- src/rt_bi_utils/rt_bi_utils/__init__.py
+++ src/rt_bi_utils/rt_bi_utils/__init__.py
@@ -1,5 +1,1 @@
-from rt_bi_utils.Geometry import AffineTransform, Geometry, LineString, MultiPolygon, Point, Polygon
-from rt_bi_utils.MinQueue import MinQueue
-from rt_bi_utils.Python import ObjectLiteral
-from rt_bi_utils.RViz import Color, KnownColors, RViz
-from rt_bi_utils.SaMsgs import SaMsgs
+"""The utility classes and functions that are common across all the packages in rt_bi suite."""
