e4a7bc418e017146eaaa6947459a3e6826aeec89
Reza Teshnizi <rht1369@gmail.com>
2023-08-23 12:41:21 -0500

    refactor(FOVUnion): Rename

--- src/rt_bi_core/rt_bi_core/ShadowTree/FovUnion.py
+++ src/rt_bi_core/rt_bi_core/ShadowTree/FovUnion.py
@@ -1,6 +1,8 @@
+from math import nan
 from typing import Dict, List, Set, Tuple, Union
 
-from rt_bi_core.Model.Sensor import Sensor
+from rt_bi_core.Model.AffineSensorRegion import AffineSensorRegion
+from rt_bi_core.Model.Track import Tracks
 from rt_bi_utils.Geometry import Geometry, LineString, MultiPolygon, Polygon
 
 Events = Tuple[Polygon, float, str, LineString]
@@ -30,46 +32,68 @@ That is, Given a tuple `(L1, L2, T1, T2)`, we get the interval of event: `[ T1 *
 
 """
 
-class Fov:
-	"""A Class to model field of view.
-	"""
-	def __init__(self, sensors):
-		self.__polygon: Union[Polygon, MultiPolygon] = None
-		self.__edges: Dict[str, LineString] = None
-		self.sensors: Dict[tuple, Sensor] = sensors
+class FovUnion:
+	"""A Class to model Field-of-View."""
+	def __init__(self):
+		self.__polygon: Union[Polygon, MultiPolygon] = Polygon()
+		self.__edges: Dict[str, LineString] = {}
+		self.__tracks: Tracks = {}
+		self.__sensors: Dict[tuple, AffineSensorRegion] = {}
 
 	def __repr__(self):
-		return "FOV-%s" % repr(self.sensors)
+		return "FOV-%s" % repr(self.__sensors)
 
 	@property
 	def polygon(self) -> Union[Polygon, MultiPolygon]:
-		"""The polygonal geometrical representation of the FOV.
-		"""
+		"""The polygonal geometrical representation of the FOV."""
 		if self.__polygon is None:
-			polygons = [self.sensors[i].region.polygon for i in self.sensors]
+			polygons = [self.__sensors[i].region.polygon for i in self.__sensors]
 			self.__polygon = Geometry.union(polygons)
 		return self.__polygon
 
+	@polygon.setter
+	def polygon(self, _: Union[Polygon, MultiPolygon]) -> None:
+		raise NotImplementedError("No setter.")
+
 	@property
-	def time(self):
-		if len(self.sensors) == 0: return None
-		for sensorId in self.sensors:
-			return self.sensors[sensorId].pose.timeNanoSecs
+	def timeNanoSec(self) -> float:
+		if len(self.__sensors) == 0: return nan
+		for sensorId in self.__sensors:
+			return self.__sensors[sensorId].centerOfRotation.timeNanoSecs
+
+	@timeNanoSec.setter
+	def timeNanoSec(self, _: float) -> None:
+		raise NotImplementedError("No setter.")
 
 	@property
 	def edges(self) -> Dict[str, LineString]:
 		if self.__edges is None:
-			for s in self.sensors:
-				self.__edges = self.__edges | self.sensors[s].region.edges
+			for s in self.__sensors:
+				self.__edges = self.__edges | self.__sensors[s].region.edges
 		return self.__edges
 
-	def __getEquivalentSensorById(self, sensorId: tuple) -> Sensor:
-		return self.sensors[(self.time, sensorId[1])]
+	@edges.setter
+	def edges(self, _: Dict[str, LineString]) -> None:
+		raise NotImplementedError("No setter.")
+
+	@property
+	def tracks(self) -> Tracks:
+		tracks: Tracks = {}
+		for sensor in self.__sensors.values():
+			tracks = {**tracks, **sensor.region.tracks}
+		return self.__tracks
+
+	@tracks.setter
+	def tracks(self, _: Tracks) -> None:
+		raise NotImplementedError("No setter.")
+
+	def __getEquivalentSensorById(self, sensorId: tuple) -> AffineSensorRegion:
+		return self.__sensors[(self.time, sensorId[1])]
 
 	def __getCollidingEdgesBySensor(self, polygon: Union[Polygon, MultiPolygon]) -> CollisionData:
 		collisionData: CollisionData = {}
-		for sensorId in self.sensors:
-			collisionData[sensorId] = self.sensors[sensorId].findCollisionsWithExtendedBb(polygon)
+		for sensorId in self.__sensors:
+			collisionData[sensorId] = self.__sensors[sensorId].findCollisionsWithExtendedBb(polygon)
 
 	def __initCollisionIntervals(self, intermediateCollisions: CollisionData) -> CollisionInterval:
 		intervals = []
@@ -81,7 +105,7 @@ class Fov:
 				intervals.append((sensorEdge, mapEdge, 0, 1))
 		return intervals
 
-	def __estimateLocationWithSlerp(self, past: "Fov", ratio: float) -> Union[Polygon, MultiPolygon]:
+	def __estimateLocationWithSlerp(self, past: "FovUnion", ratio: float) -> Union[Polygon, MultiPolygon]:
 		"""
 			#### Input
 
@@ -92,8 +116,8 @@ class Fov:
 			a `Polygon` or a `MultiPolygon` representing the state of the fov at time ratio.
 		"""
 		polygons = []
-		for sensorId in self.sensors:
-			sensor = self.sensors[sensorId]
+		for sensorId in self.__sensors:
+			sensor = self.__sensors[sensorId]
 			pastSensor = past.__getEquivalentSensorById(sensorId)
 			centerOfRotation = (self.pose.x, self.pose.y)
 			transformation = Geometry.getAffineTransformation(sensor.region.polygon, pastSensor.region.polygon, centerOfRotation)
@@ -102,7 +126,7 @@ class Fov:
 		polygons = Geometry.union(polygons)
 		return polygons
 
-	def estimateIntermediateCollisionsWithPolygon(self, past: "Fov", polygon: Union[Polygon, List[Polygon], MultiPolygon]) -> Events:
+	def estimateIntermediateCollisionsWithPolygon(self, past: "FovUnion", polygon: Union[Polygon, List[Polygon], MultiPolygon]) -> Events:
 		"""## Estimate Intermediate Collisions With Polygon
 
 			Given  and the current configuration of it (`self`),
@@ -127,8 +151,8 @@ class Fov:
 		currentCollidingEdges = self.__getCollidingEdgesBySensor(polygon)
 		pastCollidingEdges = past.__getCollidingEdgesBySensor(polygon)
 		possibleCollisionData: Events = {}
-		for sensorId in past.sensors:
-			possibleCollisionData[sensorId] = self.sensors[sensorId].findCollisionsWithExtendedBb(past.__getEquivalentSensorById(sensorId), polygon)
+		for sensorId in past.__sensors:
+			possibleCollisionData[sensorId] = self.__sensors[sensorId].findCollisionsWithExtendedBb(past.__getEquivalentSensorById(sensorId), polygon)
 		# Remove the edges that we are sure are intersecting.
 		for sensorId in currentCollidingEdges:
 			for l in currentCollidingEdges[sensorId]:
@@ -137,3 +161,6 @@ class Fov:
 			for l in pastCollidingEdges[sensorId]:
 				if sensorId in possibleCollisionData: possibleCollisionData[sensorId].remove(l)
 		collisionIntervals = self.__initCollisionIntervals(intermediateCollisions)
+
+	def addSensor(self, sensor: AffineSensorRegion) -> None:
+		return
