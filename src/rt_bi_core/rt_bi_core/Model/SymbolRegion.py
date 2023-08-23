83a66b10374cece0c86c4392e3cf42e7d68ee705
Reza Teshnizi <rht1369@gmail.com>
2023-08-23 12:41:03 -0500

    refactor(SymbolRegion): Rename

---
+++ src/rt_bi_core/rt_bi_core/Model/SymbolRegion.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from visualization_msgs.msg import Marker
+
+from rt_bi_core.Model.PolygonalRegion import PolygonalRegion
+from rt_bi_utils.Geometry import Geometry
+from rt_bi_utils.RViz import KnownColors
+
+
+class SymbolRegion(PolygonalRegion):
+	def __init__(self, name: str, coords: Geometry.CoordsList, inFov=False, **kwArgs):
+		super().__init__(
+			idNum=name,
+			boundary=coords,
+			envelopColor=KnownColors.RED if inFov else KnownColors.BLUE,
+			**kwArgs
+		)
+		self.__inFov = inFov
+
+	@property
+	def inFov(self) -> bool:
+		"""Indicates whether this region is inside the field of view."""
+		return self.__inFov
+
+	@inFov.setter
+	def inFov(self, _: bool) -> None:
+		raise NotImplementedError("No setter.")
+
+	def render(self) -> List[Marker]:
+		return super().render(renderText=True)
