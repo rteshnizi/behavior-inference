cf059ce168e62d086e31b68fee26acd440246f1d
Reza Teshnizi <rht1369@gmail.com>
2023-08-25 19:33:26 -0500

    chore: Remove dead code

--- src/rt_bi_core/rt_bi_core/Model/TeamMember.py
+++
@@ -1,7 +0,0 @@
-from rt_bi_core.Model.Agent import Agent
-
-class TeamMember(Agent):
-	def __init__(self, agentId):
-		super().__init__(agentId)
-		self.name = "T-%d" % self.agentId
-		self.isTeamMember = True
