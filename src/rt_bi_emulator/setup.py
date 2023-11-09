3a62c675b46eeccaa0ec0fe722b6ae4f2df5a624
Reza Teshnizi <rht1369@gmail.com>
2023-11-09 04:35:43 +0000

    release(0.0.5): Working CtCd for one moving region

--- src/rt_bi_emulator/setup.py
+++ src/rt_bi_emulator/setup.py
@@ -7,7 +7,7 @@ package_name = "rt_bi_emulator"
 
 setup(
 	name=package_name,
-	version="0.0.4",
+	version="0.0.5",
 	packages = find_packages(exclude=["test"]),
 	data_files=[
 		("share/ament_index/resource_index/packages", ["resource/" + package_name]),
--- src/rt_bi_utils/setup.py
+++ src/rt_bi_utils/setup.py
@@ -4,7 +4,7 @@ package_name = "rt_bi_utils"
 
 setup(
 	name=package_name,
-	version="0.0.4",
+	version="0.0.5",
 	packages=[package_name],
 	data_files=[
 		("share/ament_index/resource_index/packages",
