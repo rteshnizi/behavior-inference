345f4fe1488e84f48524fecb6b1128051604f314
Reza Teshnizi <rht1369@gmail.com>
2023-11-28 02:52:53 +0000

    release(0.0.6): Debugged CtCd

--- src/rt_bi_core/setup.py
+++ src/rt_bi_core/setup.py
@@ -7,7 +7,7 @@ package_name = "rt_bi_core"
 
 setup(
 	name=package_name,
-	version="0.0.5",
+	version="0.0.6",
 	packages = find_packages(exclude=["test", "launch"]),
 	data_files=[
 		("share/ament_index/resource_index/packages", ["resource/" + package_name]),
