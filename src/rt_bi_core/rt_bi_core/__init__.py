a5113930b3484422920f64cc470ec08a2178d8b0
Reza Teshnizi <rht1369@gmail.com>
2023-12-07 06:24:23 +0000

    chore(__init__): Remove unnecessary imports

--- src/rt_bi_core/rt_bi_core/__init__.py
+++ src/rt_bi_core/rt_bi_core/__init__.py
@@ -1,3 +1,1 @@
 """The core package of the Behavior Inference project."""
-from rt_bi_core.MapServiceInterface import MapServiceInterface
-from rt_bi_core.SensorTopicInterface import SensorTopicInterface
