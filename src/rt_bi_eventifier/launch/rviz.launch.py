99ad76bd9179adbeafb4c79fcc06832e0ebc4a56
Reza Teshnizi <rht1369@gmail.com>
2023-12-13 09:59:22 +0000

    chore(rt_bi_eventifier): Turn off CtCd rendering

--- src/rt_bi_eventifier/launch/rviz.launch.py
+++ src/rt_bi_eventifier/launch/rviz.launch.py
@@ -19,14 +19,24 @@ def generate_launch_description():
 				[os.path.join(shareDir, "config", "rviz.cg.rviz")],
 			]
 		),
+		# Node(
+		# 	package="rviz2",
+		# 	namespace="rviz2",
+		# 	executable="rviz2",
+		# 	name="rviz2_ctcd", # cspell: disable-line
+		# 	arguments=[
+		# 		"-d",
+		# 		[os.path.join(shareDir, "config", "rviz.ctcd.rviz")], # cspell: disable-line
+		# 	]
+		# ),
 		Node(
 			package="rviz2",
 			namespace="rviz2",
 			executable="rviz2",
-			name="rviz2_ctcd", # cspell: disable-line
+			name="rviz2_str", # cspell: disable-line
 			arguments=[
 				"-d",
-				[os.path.join(shareDir, "config", "rviz.ctcd.rviz")], # cspell: disable-line
+				[os.path.join(shareDir, "config", "rviz.str.rviz")], # cspell: disable-line
 			]
 		),
 	])
