b87d54387d91cb7db8ee856d5f895028a185af84
Reza Teshnizi <rht1369@gmail.com>
2024-05-17 02:35:49 +0000

    release(0.8.0): Simplified SPARQL queries and cold-start

--- src/rt_bi_commons/setup.py
+++ src/rt_bi_commons/setup.py
@@ -5,7 +5,7 @@ package_name = "rt_bi_commons"
 
 setup(
 	name=package_name,
-	version="0.7.3",
+	version="0.8.0",
 	packages = find_packages(exclude=["test"]),
 	data_files=[
 		("share/ament_index/resource_index/packages", ["resource/" + package_name]),
--- src/rt_bi_eventifier/setup.py
+++ src/rt_bi_eventifier/setup.py
@@ -7,7 +7,7 @@ packageName = "rt_bi_eventifier"
 
 setup(
 	name=packageName,
-	version="0.7.3",
+	version="0.8.0",
 	packages = find_packages(exclude=["test", "launch"]),
 	data_files=[
 		("share/ament_index/resource_index/packages", ["resource/" + packageName]),
