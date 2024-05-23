b70a828e7f2519417198e8d379da3c7617d62210
Reza Teshnizi <rht1369@gmail.com>
2024-05-23 01:48:25 +0000

    release(0.8.3): Simplify Transition Grammar

--- src/rt_bi_emulator/setup.py
+++ src/rt_bi_emulator/setup.py
@@ -7,7 +7,7 @@ package_name = "rt_bi_emulator"
 
 setup(
 	name=package_name,
-	version="0.8.2",
+	version="0.8.3",
 	packages = find_packages(exclude=["test"]),
 	data_files=[
 		("share/ament_index/resource_index/packages", ["resource/" + package_name]),
